e448d47a18b4126bef9d8907e59ce05c797cee54
addi00000 <addison@othius.com>
2022-09-14 21:33:53 -0400

    Make root page a url generation page

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import './style.scss';
 import { copyOnClick } from './funcs/copy'
 import { updateParam } from './funcs/param'
+import { generate } from './funcs/generate'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 <div class="container">
-  <p id="copy-content"></p>
+<p id="copy-content"></p>
+</div>
+<div class="generate">
+  <input type="text" id="param" placeholder="<input text here!/>"/>
+  <button id="generate">Generate</button>
 </div>
 <div class="footer">
   <span>Created by <a href="https://addidix.xyz" target="_blank">Addidix</a></span>
@@ -18,3 +23,4 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
 
 updateParam(document.querySelector<HTMLDivElement>('#copy-content')!, 'p')
 copyOnClick(document.querySelector<HTMLDivElement>('#copy-content')!)
+generate()
